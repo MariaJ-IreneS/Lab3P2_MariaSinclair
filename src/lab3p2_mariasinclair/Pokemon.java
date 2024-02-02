0c3a3fc1ea827fc4dc87b709356a6bb495b948f4
MariaJ-IreneS <misinclair15@unitec.edu>
2024-02-02 14:35:22 -0600

    1CLASE

---
+++ src/lab3p2_mariasinclair/Pokemon.java
@@ -0,0 +1,54 @@
+package lab3p2_mariasinclair;
+
+public class Pokemon {
+
+    protected String nombre;
+    protected int entrada;
+    protected String cadena; //naturaleza: Timido, Energetico, Misterioso.
+    protected boolean atrapado; //validacion y pokebola.
+
+    public Pokemon(String nombre, int entrada, String cadena, boolean atrapado) {
+        this.nombre = nombre;
+        this.entrada = entrada;
+        this.cadena = cadena;
+        this.atrapado = false;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEntrada() {
+        return entrada;
+    }
+
+    public String getCadena() {
+        return cadena;
+    }
+
+    public boolean isAtrapado() {
+        return atrapado;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEntrada(int entrada) {
+        this.entrada = entrada;
+    }
+
+    public void setCadena(String cadena) {
+        this.cadena = cadena;
+    }
+
+    public void setAtrapado(boolean atrapado) {
+        this.atrapado = atrapado;
+    }
+
+    @Override
+    public String toString() {
+        return "Pokemon: \n" + "\nNombre: " + nombre + "\nEntrada: " + entrada + "\nCadena:" + cadena + "\nAtrapado: " + atrapado;
+    }
+
+}
